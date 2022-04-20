1a78415bea0836f9531c8363a602152057358c0c
ulimy <ulim130@gmail.com>
2022-04-21 03:17:22 +0900

    exceptions 패키지 내의 코드 구현

--- src/main/java/nextstep/helloworld/mvc/exceptions/HelloAdvice.java
+++ src/main/java/nextstep/helloworld/mvc/exceptions/HelloAdvice.java
@@ -5,7 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class HelloAdvice {
+
+    @ExceptionHandler(HelloException.class)
     public ResponseEntity<String> handle() {
         return ResponseEntity.badRequest().body("HelloException");
     }
