32284b75bd7fa88d3690855f75aa295e91c75fda
ulimy <ulim130@gmail.com>
2022-04-21 17:32:29 +0900

    simpleinsert  내 코드 작성

--- src/main/java/nextstep/helloworld/jdbc/simpleinsert/SimpleInsertDao.java
+++ src/main/java/nextstep/helloworld/jdbc/simpleinsert/SimpleInsertDao.java
@@ -1,10 +1,13 @@
 package nextstep.helloworld.jdbc.simpleinsert;
 
 import nextstep.helloworld.jdbc.Customer;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.Map;
 
 @Repository
 public class SimpleInsertDao {
@@ -22,7 +25,13 @@ public class SimpleInsertDao {
      * id를 포함한 Customer 객체를 반환하세요
      */
     public Customer insertWithMap(Customer customer) {
-        return null;
+        Map<String, String> namedParameters = Map.of(
+                "first_name", customer.getFirstName(),
+                "last_name", customer.getLastName());
+
+        Number id = insertActor.executeAndReturnKey(namedParameters);
+
+        return new Customer(id.longValue() , customer.getFirstName(), customer.getLastName());
     }
 
     /**
@@ -31,6 +40,10 @@ public class SimpleInsertDao {
      * id를 포함한 Customer 객체를 반환하세요
      */
     public Customer insertWithBeanPropertySqlParameterSource(Customer customer) {
-        return null;
+        SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(customer);
+
+        Number id = insertActor.executeAndReturnKey(namedParameters);
+
+        return new Customer(id.longValue() , customer.getFirstName(), customer.getLastName());
     }
 }
